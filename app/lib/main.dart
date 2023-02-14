a84e6b3b98416a6f78ed8221a6c4f331204b65e9
MillicentMalinga <90625689+MillicentMalinga@users.noreply.github.com>
2023-02-14 20:02:30 +0200

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './take_bite.dart';
+import './login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: '',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const MyHomePage(title: ''),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +31,268 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+        backgroundColor: Colors.greenAccent,
+        body: Column(
+          children: [
+            Container(
+                height: 250.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                          "assets/images/baked-chicken-wings-asian-style-tomatoes-sauce-plate.jpg"),
+                      fit: BoxFit.cover,
+                      opacity: 0.5),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50.0),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 100.0,
+                      left: 40.0,
+                      child: Container(
+                        height: 100.0,
+                        width: 300.0,
+                        decoration: const BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(50),
+                                topLeft: Radius.circular(50))),
+                      ),
+                    ),
+                    Positioned(
+                        top: 125,
+                        left: 50,
+                        child: Column(children: [
+                          Text(
+                            'LasBite.',
+                            style: GoogleFonts.josefinSans(
+                                textStyle: const TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green,
+                            )),
+                          ),
+                          Text(
+                            'Get that last bite.',
+                          )
+                        ]))
+                  ],
+                )),
+
+            // Starting the list of restaurants
+            Expanded(
+                child: ListView(
+              children: [
+                SizedBox(
+                    height: 250,
+                    child: Stack(children: [
+                      Positioned(
+                        top: 35,
+                        left: 20,
+                        child: Material(
+                            child: Container(
+                          height: 230.0,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                          top: 0,
+                          left: 30,
+                          child: Card(
+                              elevation: 10.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Container(
+                                height: 200,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            "assets/images/anto-meneghini-IrohWzafmmA-unsplash.jpg"),
+                                        fit: BoxFit.fill)),
+                              ))),
+                      Positioned(
+                          top: 60,
+                          left: 200,
+                          child: Container(
+                              height: 150,
+                              width: 180,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Shangrila',
+                                      style: GoogleFonts.josefinSans(
+                                        textStyle: const TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      )),
+                                  Row(children: [
+                                    Icon(Icons.star, color: Colors.yellow),
+                                    Text('4.5')
+                                  ]),
+                                  Spacer(flex: 2),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SecondScreen()));
+                                    },
+                                    child: Text('Get notified'),
+                                  )
+                                ],
+                              ))),
+                    ])),
+                SizedBox(
+                    height: 250,
+                    child: Stack(children: [
+                      Positioned(
+                        top: 35,
+                        left: 20,
+                        child: Material(
+                            child: Container(
+                          height: 230.0,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                          top: 0,
+                          left: 30,
+                          child: Card(
+                              elevation: 10.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Container(
+                                height: 200,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            "assets/images/anto-meneghini-IrohWzafmmA-unsplash.jpg"),
+                                        fit: BoxFit.fill)),
+                              ))),
+                      Positioned(
+                          top: 60,
+                          left: 200,
+                          child: Container(
+                              height: 150,
+                              width: 180,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Shangrila',
+                                      style: GoogleFonts.josefinSans(
+                                        textStyle: const TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      )),
+                                  Row(children: [
+                                    Icon(Icons.star, color: Colors.yellow),
+                                    Text('4.5')
+                                  ]),
+                                  Spacer(flex: 2),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SecondScreen()));
+                                    },
+                                    child: Text('Get notified'),
+                                  )
+                                ],
+                              ))),
+                    ])),
+                SizedBox(
+                    height: 250,
+                    child: Stack(children: [
+                      Positioned(
+                        top: 35,
+                        left: 20,
+                        child: Material(
+                            child: Container(
+                          height: 230.0,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                          top: 0,
+                          left: 30,
+                          child: Card(
+                              elevation: 10.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Container(
+                                height: 200,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            "assets/images/anto-meneghini-IrohWzafmmA-unsplash.jpg"),
+                                        fit: BoxFit.fill)),
+                              ))),
+                      Positioned(
+                          top: 60,
+                          left: 200,
+                          child: Container(
+                              height: 150,
+                              width: 180,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Shangrila',
+                                      style: GoogleFonts.josefinSans(
+                                        textStyle: const TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      )),
+                                  Row(children: [
+                                    Icon(Icons.star, color: Colors.yellow),
+                                    Text('4.5')
+                                  ]),
+                                  Spacer(flex: 2),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SecondScreen()));
+                                    },
+                                    child: Text('Get notified'),
+                                  )
+                                ],
+                              ))),
+                    ])),
