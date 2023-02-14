9df2792ae9eea09b5eba90c74cbea8ad543402d6
Harriet Mwanza <90474921+HarrietMwanza@users.noreply.github.com>
2023-02-14 20:12:58 +0200

    Update signup.dart

--- app/lib/signup.dart
+++ app/lib/signup.dart
@@ -5,7 +5,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Sample App';
+  static const String _title = 'LasBite App';
 
   @override
   Widget build(BuildContext context) {
