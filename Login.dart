dd3d2bf4f63022946206ea5569886ac9651d53e8
Harriet Mwanza <90474921+HarrietMwanza@users.noreply.github.com>
2023-02-14 19:59:35 +0200

    Update Login.dart

--- Login.dart
+++ Login.dart
@@ -1,3 +1,22 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  static const String _title = 'Sample App';
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
