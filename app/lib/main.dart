2f34a8c5c028f840218ade3a59620bfb09d51a73
Harriet Mwanza <h.mwanza@alustudent.com>
2023-03-15 17:36:20 +0200

    firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import './login.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
